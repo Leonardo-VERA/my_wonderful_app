f8294705826bceccf666cd0c2981e377a5fa1155
Ray Vera <ray.leonardo.vera@gmail.com>
2025-02-06 11:35:44 +0100

    3er try de app

--- app_test.py
+++ app_test.py
@@ -1,17 +1,16 @@
-import plotly.express as px
 import streamlit as st
+import plotly.express as px
 import pandas as pd
 
-# example de dataset online 
+# Cargar datos desde URL
 url = 'https://raw.githubusercontent.com/plotly/datasets/master/gapminder2007.csv'
-
-# load data in a df data structure
 df = pd.read_csv(url)
 
+# Título de la app
 st.title('Plotly Express in Streamlit')
-# streamlit run app_test.py dans la terminal
-# pip install watchdog to auto refresh the page
 
-# make a scatter 3D plot using the dataset
+# Crear un gráfico interactivo en Streamlit
 fig = px.scatter_3d(df, x='gdpPercap', y='lifeExp', z='pop', color='continent')
-fig.show()
+
+# Mostrar gráfico en Streamlit
+st.plotly_chart(fig)  # Cambié fig.show() por st.plotly_chart(fig)
